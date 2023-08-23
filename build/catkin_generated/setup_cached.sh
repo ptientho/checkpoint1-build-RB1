8cc054c17fde29287d789721890c773440f08f54
MysterP <78469954+ptientho@users.noreply.github.com>
2023-08-23 00:04:38 -0400

    add build files 1

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/user/catkin_ws/devel:/home/user/simulation_ws/devel:/home/simulations/public_sim_ws/devel:/opt/ros/noetic:/opt/ros/noetic/share:/opt/ros/noetic/share:/home/simulations/ros2_sims_ws/install/ros1_bridge'
+export PWD='/home/user/catkin_ws/build'
